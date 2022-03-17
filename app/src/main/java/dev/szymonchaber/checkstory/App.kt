f87a2536d04ef4a9f3e7b44f2e231125820c9c05
Szymon Chaber <szymon@szymonchaber.dev>
2022-03-17 22:15:30 +0100

    Introduce Hilt

---
+++ app/src/main/java/dev/szymonchaber/checkstory/App.kt
@@ -0,0 +1,12 @@
+package dev.szymonchaber.checkstory
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
