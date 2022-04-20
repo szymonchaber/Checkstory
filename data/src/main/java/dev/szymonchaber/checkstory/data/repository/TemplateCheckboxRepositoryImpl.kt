bbaac8ef758ddfef4cf02713c6ed22264816c3bb
Szymon Chaber <szymon@szymonchaber.dev>
2022-04-20 14:22:09 +0200

    Remove unused methods from TemplateCheckboxRepository

--- data/src/main/java/dev/szymonchaber/checkstory/data/repository/TemplateCheckboxRepositoryImpl.kt
+++ data/src/main/java/dev/szymonchaber/checkstory/data/repository/TemplateCheckboxRepositoryImpl.kt
@@ -2,15 +2,8 @@ package dev.szymonchaber.checkstory.data.repository
 
 import dev.szymonchaber.checkstory.data.database.datasource.ChecklistTemplateRoomDataSource
 import dev.szymonchaber.checkstory.domain.model.checklist.template.ChecklistTemplate
-import dev.szymonchaber.checkstory.domain.model.checklist.template.ChecklistTemplateId
 import dev.szymonchaber.checkstory.domain.model.checklist.template.TemplateCheckbox
-import dev.szymonchaber.checkstory.domain.model.checklist.template.TemplateCheckboxId
 import dev.szymonchaber.checkstory.domain.repository.TemplateCheckboxRepository
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -18,20 +11,6 @@ import javax.inject.Singleton
 class TemplateCheckboxRepositoryImpl @Inject constructor(
     private val dataSource: ChecklistTemplateRoomDataSource
 ) : TemplateCheckboxRepository {
-
-    override fun createTemplateCheckbox(templateId: ChecklistTemplateId): Flow<TemplateCheckbox> {
-        return flow {
-            emit(
-                dataSource.createTemplateCheckbox(
-                    TemplateCheckbox(TemplateCheckboxId(0), null, "Checkbox 1", listOf()), templateId
-                )
-            )
-        }
-            .flowOn(Dispatchers.IO)
-            .map {
-                dataSource.getTemplateCheckbox(TemplateCheckboxId(it))
-            }
-    }
 
     override suspend fun deleteFromTemplate(checklistTemplate: ChecklistTemplate) {
         dataSource.deleteCheckboxesFromTemplate(checklistTemplate)
--- domain/src/main/java/dev/szymonchaber/checkstory/domain/repository/TemplateCheckboxRepository.kt
+++ domain/src/main/java/dev/szymonchaber/checkstory/domain/repository/TemplateCheckboxRepository.kt
@@ -1,13 +1,9 @@
 package dev.szymonchaber.checkstory.domain.repository
 
 import dev.szymonchaber.checkstory.domain.model.checklist.template.ChecklistTemplate
-import dev.szymonchaber.checkstory.domain.model.checklist.template.ChecklistTemplateId
 import dev.szymonchaber.checkstory.domain.model.checklist.template.TemplateCheckbox
-import kotlinx.coroutines.flow.Flow
 
 interface TemplateCheckboxRepository {
-
-    fun createTemplateCheckbox(templateId: ChecklistTemplateId): Flow<TemplateCheckbox>
 
     suspend fun deleteFromTemplate(checklistTemplate: ChecklistTemplate)
 
