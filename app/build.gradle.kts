13067d70be2535ae71bdcfe336c05aa87f47923e
Szymon Chaber <szymon@szymonchaber.dev>
2022-04-24 15:34:55 +0200

    Release version 0.2.1-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "dev.szymonchaber.checkstory"
         minSdk = 24
         targetSdk = 31
-        versionCode = 2
-        versionName = "0.2-alpha"
+        versionCode = 3
+        versionName = "0.2.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
