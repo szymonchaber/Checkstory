8f709e6d06027216249bb96f2148c3d6e07fb40a
Szymon Chaber <szymon@szymonchaber.dev>
2022-07-05 22:30:39 +0200

    Prevent notifications for reminders which got deleted

--- notifications/src/main/java/dev/szymonchaber/checkstory/notifications/ReminderReceiver.kt
+++ notifications/src/main/java/dev/szymonchaber/checkstory/notifications/ReminderReceiver.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.net.toUri
 import dagger.hilt.android.AndroidEntryPoint
+import dev.szymonchaber.checkstory.domain.model.checklist.template.ChecklistTemplate
 import dev.szymonchaber.checkstory.domain.model.checklist.template.ChecklistTemplateId
 import dev.szymonchaber.checkstory.domain.model.checklist.template.reminder.ReminderId
 import dev.szymonchaber.checkstory.domain.usecase.GetChecklistTemplateUseCase
@@ -33,6 +34,9 @@ class ReminderReceiver : BroadcastReceiver() {
         CoroutineScope(Dispatchers.Main)
             .launch {
                 val template = getChecklistTemplateUseCase.getChecklistTemplateOrNull(templateId) ?: return@launch
+                if (!reminderExists(reminderId, template)) {
+                    return@launch
+                }
                 val newChecklistIntent = Intent(
                     Intent.ACTION_VIEW,
                     "app://checkstory/checklist/new/${template.id.id}".toUri()
@@ -50,6 +54,12 @@ class ReminderReceiver : BroadcastReceiver() {
                     recentChecklistIntent
                 )
             }
+    }
+
+    private fun reminderExists(reminderId: ReminderId, template: ChecklistTemplate): Boolean {
+        return template.reminders.any {
+            it.id == reminderId
+        }
     }
 
     companion object {
