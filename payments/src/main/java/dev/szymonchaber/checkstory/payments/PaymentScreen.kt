baf508a6223bc3fa80b873e810aff2bd9981ec1b
Szymon Chaber <szymon@szymonchaber.dev>
2022-08-25 18:24:58 +0200

    Add feature line items and subscription plans display

--- payments/src/main/java/dev/szymonchaber/checkstory/payments/PaymentScreen.kt
+++ payments/src/main/java/dev/szymonchaber/checkstory/payments/PaymentScreen.kt
@@ -3,15 +3,36 @@ package dev.szymonchaber.checkstory.payments
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
@@ -29,7 +50,16 @@ fun PaymentScreen(navigator: DestinationsNavigator) {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = stringResource(R.string.checkstory))
+                    Text(text = stringResource(R.string.upgrade))
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navigator.navigateUp()
+                        }
+                    ) {
+                        Icon(Icons.Filled.ArrowBack, "")
+                    }
                 },
                 elevation = 12.dp
             )
@@ -42,12 +72,94 @@ fun PaymentScreen(navigator: DestinationsNavigator) {
 @Composable
 fun PaymentView(viewModel: PaymentViewModel) {
     val state by viewModel.state.collectAsState(initial = PaymentState.initial)
-
     val context = LocalContext.current
     Column {
-        Text(text = state.result)
+        Text(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .padding(top = 16.dp),
+            text = "Unlock Checkstory Pro",
+            style = MaterialTheme.typography.h5
+        )
+        Text(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .padding(top = 8.dp),
+            text = "By upgrading, you get access to the full package:"
+        )
+        FeatureLine("Ads-free experience")
+        FeatureLine("Unlimited templates")
+        FeatureLine("Unlimited reminders")
+        FeatureLine("Unlimited history")
+        FeatureLine("Synchronization with the web app (soon)")
+
+        PaymentsPlans()
+        Text(
+            modifier = Modifier.padding(horizontal = 20.dp),
+            text = state.result
+        )
         TextButton(onClick = { viewModel.onEvent(PaymentEvent.BuyClicked(context.getActivity()!!)) }) {
             Text(text = "Buy pro version")
+        }
+    }
+}
+
+@Composable
+fun PaymentsPlans() {
+    Row(
+        modifier = Modifier
+            .padding(horizontal = 20.dp)
+            .padding(top = 24.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        SubscriptionPlan("1\nmonth", "$8.99", "$8.99/mo")
+        SubscriptionPlan("12\nmonths", "$85,99", "$6.99/mo") // save 20%
+        SubscriptionPlan("3\nmonths", "$25,99", "$8.69/mo") // save 3%
+    }
+}
+
+@Composable
+fun RowScope.SubscriptionPlan(header: String, price: String, pricePerMonth: String) {
+    Card(
+        modifier = Modifier
+            .weight(1f),
+        backgroundColor = MaterialTheme.colors.primary
+    ) {
+        Column(
+            Modifier
+                .padding(horizontal = 16.dp)
+                .padding(top = 12.dp, bottom = 16.dp)
+        ) {
+            Text(
+                modifier = Modifier.align(alignment = Alignment.CenterHorizontally),
+                text = header,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Text(
+                modifier = Modifier
+                    .padding(top = 16.dp, bottom = 8.dp)
+                    .align(alignment = Alignment.CenterHorizontally),
+                text = price,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+            Box(
+                modifier =
+                Modifier
+                    .background(Color.Gray)
+                    .fillMaxWidth()
+                    .height(1.dp)
+                    .padding(top = 8.dp)
+            )
+            Text(
+                modifier = Modifier
+                    .padding(top = 8.dp)
+                    .align(alignment = Alignment.CenterHorizontally),
+                text = pricePerMonth,
+//                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -56,4 +168,30 @@ fun Context.getActivity(): Activity? = when (this) {
     is Activity -> this
     is ContextWrapper -> baseContext.getActivity()
     else -> null
+}
+
+@Composable
+fun FeatureLine(text: String) {
+    Row {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 20.dp)
+        ) {
+            Checkbox(
+                modifier = Modifier.align(Alignment.CenterVertically),
+                checked = true,
+                interactionSource = MutableInteractionSource(),
+                onCheckedChange = {
+                    // nop
+                }
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterVertically),
+                text = text,
+            )
+        }
+    }
 }
