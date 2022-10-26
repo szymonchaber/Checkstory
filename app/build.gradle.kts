0dc2503e5d17fba5cbf834deac6d8666e3f77c0b
Szymon Chaber <szymon@szymonchaber.dev>
2022-10-26 11:52:25 +0200

    Release version 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.szymonchaber.checkstory"
         minSdk = 24
         targetSdk = 31
-        versionCode = 22
-        versionName = "1.1.1"
+        versionCode = 23
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
