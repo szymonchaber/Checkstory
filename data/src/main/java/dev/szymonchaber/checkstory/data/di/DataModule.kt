bd325bb3fbc4175a3956da81a37796da89a77d43
Szymon Chaber <szymon@szymonchaber.dev>
2022-12-20 11:42:11 +0100

    Prepare Ktor HttpClient in DI

--- data/src/main/java/dev/szymonchaber/checkstory/data/di/DataModule.kt
+++ data/src/main/java/dev/szymonchaber/checkstory/data/di/DataModule.kt
@@ -11,7 +11,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import dev.szymonchaber.checkstory.data.repository.ChecklistRepositoryImpl
-import dev.szymonchaber.checkstory.data.repository.ChecklistTemplateRepositoryImpl
+import dev.szymonchaber.checkstory.data.repository.RemoteChecklistTemplateRepository
 import dev.szymonchaber.checkstory.data.repository.TemplateCheckboxRepositoryImpl
 import dev.szymonchaber.checkstory.data.repository.TemplateReminderRepositoryImpl
 import dev.szymonchaber.checkstory.domain.repository.ChecklistRepository
@@ -20,6 +20,20 @@ import dev.szymonchaber.checkstory.domain.repository.TemplateCheckboxRepository
 import dev.szymonchaber.checkstory.domain.repository.TemplateReminderRepository
 import javax.inject.Named
 import javax.inject.Singleton
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.features.DefaultRequest
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.KotlinxSerializer
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.features.observer.ResponseObserver
+import io.ktor.client.request.header
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import kotlinx.serialization.json.Json
+import timber.log.Timber
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -45,6 +59,44 @@ interface DataModule {
         fun provideOnboardingPreferences(application: Application): DataStore<Preferences> {
             return PreferenceDataStoreFactory.create {
                 application.preferencesDataStoreFile("onboarding_preferences")
+            }
+        }
+
+        private const val TIME_OUT = 60_000
+
+        @Provides
+        fun provideHttpClient(): HttpClient {
+            return HttpClient(Android) {
+                install(JsonFeature) {
+                    serializer = KotlinxSerializer(Json {
+                        prettyPrint = true
+                        isLenient = true
+                        ignoreUnknownKeys = true
+                    })
+                }
+                engine {
+                    connectTimeout = TIME_OUT
+                    socketTimeout = TIME_OUT
+                }
+
+                install(Logging) {
+                    logger = object : Logger {
+                        override fun log(message: String) {
+                            Timber.v(message)
+                        }
+                    }
+                    level = LogLevel.ALL
+                }
+
+                install(ResponseObserver) {
+                    onResponse { response ->
+                        Timber.d("HTTP status: ${response.status.value}")
+                    }
+                }
+
+                install(DefaultRequest) {
+                    header(HttpHeaders.ContentType, ContentType.Application.Json)
+                }
             }
         }
     }
