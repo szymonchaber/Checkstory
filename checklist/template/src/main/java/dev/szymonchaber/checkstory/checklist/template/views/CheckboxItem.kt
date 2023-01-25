508d5cf591f29fff1fa9edafd38d9fbf272d7efc
Szymon Chaber <szymon@szymonchaber.dev>
2023-01-25 11:50:23 +0100

    Adjust drag handle color

--- checklist/template/src/main/java/dev/szymonchaber/checkstory/checklist/template/views/CheckboxItem.kt
+++ checklist/template/src/main/java/dev/szymonchaber/checkstory/checklist/template/views/CheckboxItem.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -80,6 +81,7 @@ private fun RowScope.DragHandle(state: ReorderableLazyListState) {
             .detectReorder(state)
             .align(Alignment.CenterVertically),
         painter = painterResource(id = R.drawable.drag_indicator),
+        tint = Color.Gray,
         contentDescription = null
     )
 }
