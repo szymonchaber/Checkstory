92c5fa85e3729aaa46e53f54f93c6fef74106855
Szymon Chaber <szymon@szymonchaber.dev>
2023-03-03 14:47:13 +0100

    Change section label color to dark gray

--- design/src/main/java/dev/szymonchaber/checkstory/design/views/SectionLabel.kt
+++ design/src/main/java/dev/szymonchaber/checkstory/design/views/SectionLabel.kt
@@ -6,13 +6,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SectionLabel(text: String, modifier: Modifier = Modifier) {
     Text(
         modifier = modifier,
         style = MaterialTheme.typography.caption.copy(fontWeight = FontWeight.Medium),
-        color = Color.Gray,
+        color = Color.DarkGray,
         text = text,
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SectionLabelPreview() {
+    SectionLabel(text = "Items")
 }
