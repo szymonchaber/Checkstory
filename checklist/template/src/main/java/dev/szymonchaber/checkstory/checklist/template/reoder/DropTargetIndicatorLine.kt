372c0dfead195c9aed4f991daa3ff8249db8e132
Szymon Chaber <szymon@szymonchaber.dev>
2023-03-21 14:02:19 +0100

    Make target line snap into position on first target during a drag

--- checklist/template/src/main/java/dev/szymonchaber/checkstory/checklist/template/reoder/DropTargetIndicatorLine.kt
+++ checklist/template/src/main/java/dev/szymonchaber/checkstory/checklist/template/reoder/DropTargetIndicatorLine.kt
@@ -1,31 +1,45 @@
 package dev.szymonchaber.checkstory.checklist.template.reoder
 
 import androidx.compose.animation.core.animateOffsetAsState
+import androidx.compose.animation.core.snap
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun DropTargetIndicatorLine() {
     val state = LocalDragDropState.current
 
-    val targetValue = LocalDensity.current.run {
-        (state.currentDropTargetInfo?.offset
-            ?: Offset.Zero) - Offset.Zero.copy(y = 48.dp.toPx()) // TODO more debug lines still?
+    val offsetY = LocalDensity.current.toPx(48.dp) // TODO more debug lines still?
+    val targetValue = state.currentDropTargetInfo?.offset?.minus(Offset(x = 0f, y = offsetY)) ?: Offset.Zero
+
+    val shouldSnap = remember(state.previousDropTargetInfo) {
+        state.previousDropTargetInfo == null
     }
-    val offset by animateOffsetAsState(targetValue = targetValue)
+    val offset by animateOffsetAsState(
+        targetValue = targetValue,
+        animationSpec = if (shouldSnap) {
+            snap()
+        } else {
+            spring()
+        }
+    )
     if (state.isDragging) {
         Canvas(
             modifier = Modifier
-                .padding(end = 20.dp)
+                .padding(end = 16.dp)
                 .fillMaxWidth()
                 .graphicsLayer {
                     translationY = offset.y
@@ -39,4 +53,8 @@ fun DropTargetIndicatorLine() {
             )
         }
     }
+}
+
+private fun Density.toPx(dp: Dp): Float {
+    return dp.toPx()
 }
