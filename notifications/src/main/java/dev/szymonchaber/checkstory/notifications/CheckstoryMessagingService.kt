4b0f7d559d7acd115a290ea12eb06a0e06f53fa2
Szymon Chaber <szymon@szymonchaber.dev>
2023-05-31 12:27:08 +0200

    Login on Firebase message, as it's now sent on purchase status change

--- notifications/src/main/java/dev/szymonchaber/checkstory/notifications/CheckstoryMessagingService.kt
+++ notifications/src/main/java/dev/szymonchaber/checkstory/notifications/CheckstoryMessagingService.kt
@@ -5,11 +5,13 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import dagger.hilt.android.AndroidEntryPoint
 import dev.szymonchaber.checkstory.data.synchronization.SynchronizationWorker
+import dev.szymonchaber.checkstory.domain.usecase.LoginUseCase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class CheckstoryMessagingService : FirebaseMessagingService() {
@@ -17,12 +19,16 @@ class CheckstoryMessagingService : FirebaseMessagingService() {
     private val job = SupervisorJob()
     private val scope = CoroutineScope(Dispatchers.IO + job)
 
+    @Inject
+    lateinit var loginUseCase: LoginUseCase
+
     override fun onNewToken(token: String) = Unit // TODO handle when we get actual backend
 
     override fun onMessageReceived(message: RemoteMessage) {
         Timber.e("Got message! ${message.notification}")
         scope.launch {
             SynchronizationWorker.scheduleExpeditedUnlessOngoing(WorkManager.getInstance(application))
+            loginUseCase.login()
         }
     }
 
