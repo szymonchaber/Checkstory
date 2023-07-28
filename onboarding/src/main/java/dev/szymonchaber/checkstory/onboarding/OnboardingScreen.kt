52b2c73cbf4c10968c722b80014443c059467c7d
Szymon Chaber <szymon@szymonchaber.dev>
2023-07-29 01:07:08 +0200

    Include legal links in Onboarding flow

--- onboarding/src/main/java/dev/szymonchaber/checkstory/onboarding/OnboardingScreen.kt
+++ onboarding/src/main/java/dev/szymonchaber/checkstory/onboarding/OnboardingScreen.kt
@@ -4,28 +4,45 @@ import androidx.activity.compose.BackHandler
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -89,14 +106,16 @@ fun OnboardingScreen(
                     pagerState = pagerState,
                     modifier = Modifier
                         .align(Alignment.CenterHorizontally)
-                        .padding(16.dp),
+                        .padding(horizontal = 16.dp)
+                        .padding(vertical = 8.dp),
                 )
+                LegalLinks()
                 Button(
                     modifier = Modifier
                         .fillMaxWidth()
                         .align(Alignment.CenterHorizontally)
                         .padding(horizontal = 16.dp)
-                        .padding(bottom = 16.dp),
+                        .padding(vertical = 8.dp),
                     onClick = {
                         when (pagerState.currentPage) {
                             0 -> {
@@ -104,6 +123,7 @@ fun OnboardingScreen(
                                     pagerState.animateScrollToPage(1)
                                 }
                             }
+
                             1 -> {
                                 navigator.navigate(Routes.onboardingTemplateScreen()) {
                                     popUpTo(Routes.homeScreen())
@@ -117,6 +137,61 @@ fun OnboardingScreen(
             }
         }
     )
+}
+
+@Composable
+private fun LegalLinks() {
+    val uriHandler = LocalUriHandler.current
+    Row(
+        modifier = Modifier
+            .height(IntrinsicSize.Min)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        var privacyTextSize by remember {
+            mutableStateOf(0.dp)
+        }
+        var termsTextSize by remember {
+            mutableStateOf(0.dp)
+        }
+        val density = LocalDensity.current
+        Text(
+            modifier = Modifier
+                .defaultMinSize(minWidth = termsTextSize)
+                .onGloballyPositioned {
+                    density.apply {
+                        privacyTextSize = it.size.width.toDp()
+                    }
+                }
+                .clickable {
+                    uriHandler.openUri("https://checkstory.tech/privacy")
+                },
+            style = MaterialTheme.typography.caption.copy(color = Color.DarkGray),
+            text = "Privacy",
+            textDecoration = TextDecoration.Underline
+        )
+        Divider(
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+                .fillMaxHeight()
+                .width(1.dp)
+        )
+        Text(
+            modifier = Modifier
+                .defaultMinSize(minWidth = privacyTextSize)
+                .onGloballyPositioned {
+                    density.apply {
+                        termsTextSize = it.size.width.toDp()
+                    }
+                }
+                .clickable {
+                    uriHandler.openUri("https://checkstory.tech/terms-of-service")
+                },
+            style = MaterialTheme.typography.caption.copy(color = Color.DarkGray),
+            text = "Terms",
+            textDecoration = TextDecoration.Underline
+        )
+    }
 }
 
 @OptIn(ExperimentalPagerApi::class)
