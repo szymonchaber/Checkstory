9ec5dfc45531726f9b26cefb9acdff8724ceaab5
Szymon Chaber <szymonkchaber@gmail.com>
2024-05-01 15:48:07 +0200

    Remember done/undone task count calculation

--- design/src/main/java/dev/szymonchaber/checkstory/design/views/CheckedItemsRatio.kt
+++ design/src/main/java/dev/szymonchaber/checkstory/design/views/CheckedItemsRatio.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -23,10 +24,12 @@ fun CheckedItemsRatio(
     Row(
         modifier = modifier
     ) {
-        val flattenedItems = checklist.flattenedItems
-        val checkedCount = flattenedItems.checkedCount()
-        val allCount = flattenedItems.count()
-        val ratio = "$checkedCount/$allCount"
+        val ratio = remember(checklist) {
+            val flattenedItems = checklist.flattenedItems
+            val checkedCount = flattenedItems.checkedCount()
+            val allCount = flattenedItems.count()
+            "$checkedCount/$allCount"
+        }
         Text(
             modifier = Modifier.align(Alignment.CenterVertically),
             style = MaterialTheme.typography.caption,
